23ce61f1a080abd3a7194033e73e5584bcf8b669
Chad-Mowbray <mowbray.chad@gmail.com>
2021-03-23 10:33:02 -0500

    add warning

--- src/Header.js
+++ src/Header.js
@@ -8,6 +8,7 @@ class Header extends Component {
       <div >
         <h3>Graphical Search</h3>
           <h5>Draw an "X" or an "O" with your mouse, and I will try to guess which one you drew:</h5>
+          <h6>Note: the first use will take around 10 seconds</h6>
       </div>
     )
   }
